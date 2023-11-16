fe3b7ad58305c084516a3a1f54f3f3e4ed6dfba4
andres1602 <liamgsc20@gmail.com>
2023-11-15 21:53:28 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const div = document.querySelector(".caja-2")
+
+console.log(div)
+
+
+
+const PI = 3.1416
+let edad = 18;
+
+if(edad > 18){
+    console.log("Eres mayor de edad")
+
+}else{
+    console.log("No eres mayor de edad")
+}
+
+//for(let index = 0; index < 10; index++){
+    //console.log(index)
+//}
+
+contador = 1;
+
+//while(contador < 10){
+    //contador++
+    //console.log(contador)
+//}
+
+do {
+  contador++;
+  console.log(contador);  
+} while (contador < 6);
